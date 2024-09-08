864fa1bf21ae2ffe1006828527b05cbf23a7f3a0
Fabian Gaufillet <fabian.gaufillet@gmail.com>
2024-09-08 16:28:49 +0200

    Customize the details page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {HousingService} from "../housing.service";
+import {Housinglocation} from "../housinglocation";
 
 @Component({
   selector: 'app-details',
@@ -8,5 +11,14 @@ import { Component } from '@angular/core';
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
+  housingLocationId = -1;
+  housingLocation: Housinglocation | undefined;
+
+  constructor(private route: ActivatedRoute, private housingService: HousingService) {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
+  }
+
+
 
 }
--- src/app/housinglocation/housinglocation.component.ts
+++ src/app/housinglocation/housinglocation.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Housinglocation } from "../housinglocation";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-housinglocation',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './housinglocation.component.html',
   styleUrl: './housinglocation.component.css'
 })
