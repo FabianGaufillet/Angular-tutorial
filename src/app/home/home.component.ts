fa73f1ef6f7b40a9855bbe11d32c4e6fdb972551
Fabian Gaufillet <fabian.gaufillet@gmail.com>
2024-09-08 14:38:27 +0200

    Create housing location component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HousinglocationComponent } from "../housinglocation/housinglocation.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HousinglocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
